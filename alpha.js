e1393ec885cf9ac2a8ff9bcf35b3fa0832665431
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-10-09 17:57:28 +0530

    Files added

--- alpha.js
+++ alpha.js
@@ -2,6 +2,10 @@ const getQuotes = async() => {
     const api="https://type.fit/api/quotes";
     try
     {
+        let data =await fetch(api);
+        console.log(data.json());
     }catch(error)
     {}
 };
+
+getQuotes();
